6a4710c742390653834fba7f9930d69d430affe5
yangsijun <sijun0905@gmail.com>
2022-11-03 04:27:11 +0900

    Add 'No posts yet' Message

--- lib/my_posts_page.dart
+++ lib/my_posts_page.dart
@@ -10,7 +10,47 @@ class MyPostsPage extends StatefulWidget {
   State<MyPostsPage> createState() => _MyPostsPageState();
 }
 
+
 class _MyPostsPageState extends State<MyPostsPage> {
+  int _myPostsNum = 0;
+
+  List<Widget> getMyPostsList(BuildContext context) {
+    List<Widget> myPostsList = [];
+    for (int i = 0; i < studentVoicePostList.length; i++) {
+      if (studentVoicePostList[i].writer == myUser.nickname) {
+        _myPostsNum++;
+        myPostsList.add(
+          Container(
+            margin: const EdgeInsets.only(left: 10, right: 10),
+            color: Theme.of(context).colorScheme.surface,
+            child: Column(
+              children: [
+                Column(
+                  children: [
+                    ListTile(
+                      // leading: const Icon(Icons.restaurant),
+                      title: Text(studentVoicePostList[i].title),
+                      subtitle: Text(studentVoicePostList[i].writer),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => OneStudentVoicePage(i),
+                          ),
+                        ).then((value) => setState(() {}));
+                      },
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
+      }
+    }
+    return myPostsList;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,27 +70,14 @@ class _MyPostsPageState extends State<MyPostsPage> {
             child: Container(
               margin: const EdgeInsets.only(left: 20, right: 20),
               child: Column(
-                children: [
-                  for(int i = 0; i < studentVoicePostList.length; i++)
-                    if (studentVoicePostList[i].writer == myUser.nickname)
-                      Column(
-                        children: [
-                          ListTile(
-                            title: Text(studentVoicePostList[i].title),
-                            subtitle: Text(studentVoicePostList[i].writer),
-                            onTap: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => OneStudentVoicePage(i),
-                                ),
-                              ).then((value) => setState(() {}));
-                            },
-                          ),
-                          const Divider(),
-                        ],
-                      ),
-                ],
+                children: (_myPostsNum > 0)
+                  ? getMyPostsList(context)
+                  : [
+                    Container(
+                      margin: const EdgeInsets.only(top: 20),
+                      child: const Text('No posts yet'),
+                    ),
+                  ],
               ),
             ),
           ),
