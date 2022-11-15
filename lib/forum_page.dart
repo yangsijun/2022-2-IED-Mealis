cea5c14cb32153ba55219188973848017a628109
Yang Sijun <sijun0905@gmail.com>
2022-11-15 19:44:00 +0900

    Forum page color fix

--- lib/forum_page.dart
+++ lib/forum_page.dart
@@ -53,28 +53,35 @@ class _ForumPageState extends State<ForumPage> {
                               Navigator.push(
                                 context,
                                 MaterialPageRoute(
-                                  builder: (context) => const AnnouncementPage(),
+                                  builder: (context) =>
+                                      const AnnouncementPage(),
                                 ),
                               ).then((value) => setState(() {}));
                             },
                           ),
                           Column(
                             children: [
-                              for (int i = 0; i < showRecentAnnouncementNum; i++)
+                              for (int i = 0;
+                                  i < showRecentAnnouncementNum;
+                                  i++)
                                 Container(
-                                  margin: const EdgeInsets.only(left: 10, right: 10),
+                                  margin: const EdgeInsets.only(
+                                      left: 10, right: 10),
                                   color: Theme.of(context).colorScheme.surface,
                                   child: Column(
                                     children: [
                                       ListTile(
                                         // leading: const Icon(Icons.restaurant),
-                                        title: Text(announcementPostList[i].title),
-                                        subtitle: Text(announcementPostList[i].writer),
+                                        title:
+                                            Text(announcementPostList[i].title),
+                                        subtitle: Text(
+                                            announcementPostList[i].writer),
                                         onTap: () {
                                           Navigator.push(
                                             context,
                                             MaterialPageRoute(
-                                              builder: (context) => OneAnnouncementPage(i),
+                                              builder: (context) =>
+                                                  OneAnnouncementPage(i),
                                             ),
                                           ).then((value) => setState(() {}));
                                         },
@@ -102,72 +109,144 @@ class _ForumPageState extends State<ForumPage> {
                               Navigator.push(
                                 context,
                                 MaterialPageRoute(
-                                  builder: (context) => const StudentVoicePage(),
+                                  builder: (context) =>
+                                      const StudentVoicePage(),
                                 ),
                               ).then((value) => setState(() {}));
                             },
                           ),
                           Column(
                             children: [
-                              for (int i = 0; i < showRecentStudentVoiceNum; i++)
+                              for (int i = 0;
+                                  i < showRecentStudentVoiceNum;
+                                  i++)
                                 Container(
-                                  margin: const EdgeInsets.only(left: 10, right: 10),
+                                  margin: const EdgeInsets.only(
+                                      left: 10, right: 10),
                                   color: Theme.of(context).colorScheme.surface,
                                   child: Column(
                                     children: [
                                       ListTile(
                                         // leading: const Icon(Icons.restaurant),
-                                        title: Text(studentVoicePostList[i].title),
-                                        subtitle: Text(studentVoicePostList[i].writer),
+                                        title:
+                                            Text(studentVoicePostList[i].title),
+                                        subtitle: Text(
+                                            studentVoicePostList[i].writer),
                                         onTap: () {
                                           Navigator.push(
                                             context,
                                             MaterialPageRoute(
-                                              builder: (context) => OneStudentVoicePage(i),
+                                              builder: (context) =>
+                                                  OneStudentVoicePage(i),
                                             ),
                                           ).then((value) => setState(() {}));
                                         },
                                       ),
                                       Row(
-                                        mainAxisAlignment: MainAxisAlignment.end,
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.end,
                                         children: [
                                           ButtonBar(
                                             alignment: MainAxisAlignment.center,
                                             children: <Widget>[
                                               ActionChip(
-                                                avatar: Icon(Icons.thumb_up, color: (myEvalForStudentVoicePostList[i].isLiked) ? Colors.blueAccent : Theme.of(context).chipTheme.surfaceTintColor),
-                                                label: Text(studentVoicePostList[i].like.toString(), style: TextStyle(color: (myEvalForStudentVoicePostList[i].isLiked) ? Colors.blueAccent : Theme.of(context).chipTheme.surfaceTintColor, fontSize: 14)),
+                                                avatar: Icon(Icons.thumb_up,
+                                                    color: (myEvalForStudentVoicePostList[
+                                                                i]
+                                                            .isLiked)
+                                                        ? Colors.redAccent
+                                                        : Theme.of(context)
+                                                            .chipTheme
+                                                            .surfaceTintColor),
+                                                label: Text(
+                                                    studentVoicePostList[i]
+                                                        .like
+                                                        .toString(),
+                                                    style: TextStyle(
+                                                        color: (myEvalForStudentVoicePostList[
+                                                                    i]
+                                                                .isLiked)
+                                                            ? Colors.redAccent
+                                                            : Theme.of(context)
+                                                                .chipTheme
+                                                                .surfaceTintColor,
+                                                        fontSize: 14)),
                                                 onPressed: () {
                                                   setState(() {
-                                                    if (myEvalForStudentVoicePostList[i].isLiked) {
-                                                      studentVoicePostList[i].like--;
-                                                      myEvalForStudentVoicePostList[i].isLiked = false;
+                                                    if (myEvalForStudentVoicePostList[
+                                                            i]
+                                                        .isLiked) {
+                                                      studentVoicePostList[i]
+                                                          .like--;
+                                                      myEvalForStudentVoicePostList[
+                                                              i]
+                                                          .isLiked = false;
                                                     } else {
-                                                      if (myEvalForStudentVoicePostList[i].isDisliked) {
-                                                        studentVoicePostList[i].dislike--;
-                                                        myEvalForStudentVoicePostList[i].isDisliked = false;
+                                                      if (myEvalForStudentVoicePostList[
+                                                              i]
+                                                          .isDisliked) {
+                                                        studentVoicePostList[i]
+                                                            .dislike--;
+                                                        myEvalForStudentVoicePostList[
+                                                                i]
+                                                            .isDisliked = false;
                                                       }
-                                                      studentVoicePostList[i].like++;
-                                                      myEvalForStudentVoicePostList[i].isLiked = true;
+                                                      studentVoicePostList[i]
+                                                          .like++;
+                                                      myEvalForStudentVoicePostList[
+                                                              i]
+                                                          .isLiked = true;
                                                     }
                                                   });
                                                 },
                                               ),
                                               ActionChip(
-                                                avatar: Icon(Icons.thumb_down, color: (myEvalForStudentVoicePostList[i].isDisliked) ? Colors.redAccent : Theme.of(context).chipTheme.surfaceTintColor),
-                                                label: Text(studentVoicePostList[i].dislike.toString(), style: TextStyle(color: (myEvalForStudentVoicePostList[i].isDisliked) ? Colors.redAccent : Theme.of(context).chipTheme.surfaceTintColor, fontSize: 14)),
+                                                avatar: Icon(Icons.thumb_down,
+                                                    color: (myEvalForStudentVoicePostList[
+                                                                i]
+                                                            .isDisliked)
+                                                        ? Colors.blueAccent
+                                                        : Theme.of(context)
+                                                            .chipTheme
+                                                            .surfaceTintColor),
+                                                label: Text(
+                                                    studentVoicePostList[i]
+                                                        .dislike
+                                                        .toString(),
+                                                    style: TextStyle(
+                                                        color: (myEvalForStudentVoicePostList[
+                                                                    i]
+                                                                .isDisliked)
+                                                            ? Colors.blueAccent
+                                                            : Theme.of(context)
+                                                                .chipTheme
+                                                                .surfaceTintColor,
+                                                        fontSize: 14)),
                                                 onPressed: () {
                                                   setState(() {
-                                                    if (myEvalForStudentVoicePostList[i].isDisliked) {
-                                                      studentVoicePostList[i].dislike--;
-                                                      myEvalForStudentVoicePostList[i].isDisliked = false;
+                                                    if (myEvalForStudentVoicePostList[
+                                                            i]
+                                                        .isDisliked) {
+                                                      studentVoicePostList[i]
+                                                          .dislike--;
+                                                      myEvalForStudentVoicePostList[
+                                                              i]
+                                                          .isDisliked = false;
                                                     } else {
-                                                      if (myEvalForStudentVoicePostList[i].isLiked) {
-                                                        studentVoicePostList[i].like--;
-                                                        myEvalForStudentVoicePostList[i].isLiked = false;
+                                                      if (myEvalForStudentVoicePostList[
+                                                              i]
+                                                          .isLiked) {
+                                                        studentVoicePostList[i]
+                                                            .like--;
+                                                        myEvalForStudentVoicePostList[
+                                                                i]
+                                                            .isLiked = false;
                                                       }
-                                                      studentVoicePostList[i].dislike++;
-                                                      myEvalForStudentVoicePostList[i].isDisliked = true;
+                                                      studentVoicePostList[i]
+                                                          .dislike++;
+                                                      myEvalForStudentVoicePostList[
+                                                              i]
+                                                          .isDisliked = true;
                                                     }
                                                   });
                                                 },
